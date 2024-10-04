02f5fa2acf4f41973dbf1194d91f76aebce1c555
Pablo Barra Lillo <pablo.barra@inmente.cl>
2024-10-03 21:52:20 -0300

    UPDATE_202410032152

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 const helmet = require("helmet");
 
 const app = express();
@@ -10,7 +10,7 @@ app.set("port", process.env.PORT || 3002);
 
 app.use(helmet()); // Protege con cabeceras de seguridad
 app.use(express.json()); // Parseo de JSON en las peticiones
-//app.use(cors());
+app.use(cors());
 app.use(router);
 
 app.listen(app.get("port"), () => {
